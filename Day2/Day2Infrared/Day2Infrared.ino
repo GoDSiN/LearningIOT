f9f4b5c8f58fbb677202498a2c136e93a8665699
aomms-hub <126011747+aomms-hub@users.noreply.github.com>
2023-03-30 22:19:24 +0700

    in class code

--- Day2/Day2Infrared/Day2Infrared.ino
+++ Day2/Day2Infrared/Day2Infrared.ino
@@ -1,17 +1,18 @@
 const int e18Pin = 2;
 int statusVal = 0;
 void setup() {
-  // put your setup code here, to run once:
+
   pinMode(e18Pin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  statusVal = digital.Read(e18Pin);
+
+  statusVal = digitalRead(e18Pin);
   if(statusVal == LOW){
     Serial.println("Collision Detected.");
-  }else{
+  } else{
     Serial.println("No Collision Detected.");
   }
+  delay(1000);
 }
