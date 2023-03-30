51deb0fb5d892d204879fe487edb25edb454bae6
aomms-hub <126011747+aomms-hub@users.noreply.github.com>
2023-03-30 22:22:27 +0700

    in class code

--- Day2/Day2Ultrasonic2/Day2Ultrasonic2.ino
+++ Day2/Day2Ultrasonic2/Day2Ultrasonic2.ino
@@ -1,65 +1,66 @@
-const unsigned int TRIG_PIN2=2;
-const unsigned int TRIG_PIN3=3;
-const unsigned int TRIG_PIN4=4;
-const unsigned int TRIG_PIN5=5;
-const unsigned int ECHO_PIN=2;
+const unsigned int TRIG_PIN=2;
+const unsigned int ECHO_PIN=4;
+
+int ledPin8 = 8;
+int ledPin9 = 9;
+int ledPin10 = 10;
+int ledPin11 = 11;
 //const unsigned int BAUD_RATE=9600;
 void setup() {
-  // put your setup code here, to run once:
+  //put your setup code here, to run once:
   //Serial.begin(BAUD_RATE);
   Serial.begin(9600);
-  pinMode(TRIG_PIN2, OUTPUT);
-  pinMode(TRIG_PIN3, OUTPUT);
-  pinMode(TRIG_PIN4, OUTPUT);
-  pinMode(TRIG_PIN5, OUTPUT);
+  pinMode(ledPin8, OUTPUT); //sets the pin as output
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
+  pinMode(ledPin11, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // ระยะห่างสูงสุด 450 CM = 450*29.1 = 13095/74 = 176.96
-  // 0-25% = (176.96/100 * 25) = 44.24 จำนวนเต็ม = 45
-  // 26-50% = (176.96/100 * 50)= 88.48 จำนวนเต็ม = 89
-  // 51-75% = (176.96/100 * 75)= 132.72 จำนวนเต็ม =133
-  // 76-100% = 176.96 จำนวนเต็ม = 177
+  long duration, distance, max_length ;
+  max_length = 100; // หน่วยเป็น cm
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
 
   duration = pulseIn(ECHO_PIN, HIGH);
-  distance = (duration/2) / 74; //แปลงเป็น CM ถ้าแปลงเป็น ince (duration/2)/74\
-  
-  if(distance < 45){ //ค่า 300 สามารถกำหนดปรับได้ตามค่าแสงห้องต่างๆ
-    digitalWrite(TRIG_PIN2, HIGH);
-    digitalWrite(TRIG_PIN3, LOW);
-    digitalWrite(TRIG_PIN4, LOW);
-    digitalWrite(TRIG_PIN5, LOW);
-    Serial.println("LED2 ON");
-    Serial.println();
+  distance = (duration/2) / 29.1; //แปลงเป็น CM ถ้าแปลงเป็น ince (duration/2)/74
+
+  if(distance <= 25){
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, LOW);
+    digitalWrite(ledPin10, LOW);
+    digitalWrite(ledPin11, LOW);
   }
-  else if(distance < 89){
-    digitalWrite(TRIG_PIN2, LOW);
-    digitalWrite(TRIG_PIN3, HIGH);
-    digitalWrite(TRIG_PIN4, LOW);
-    digitalWrite(TRIG_PIN5, LOW);
-    Serial.println("LED3 ON");
-    Serial.println();
+
+  else if(distance > 25 && distance <= 50){
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, HIGH);
+    digitalWrite(ledPin10, LOW);
+    digitalWrite(ledPin11, LOW);
   }
-  else if(distance < 133){
-    digitalWrite(TRIG_PIN2, LOW);
-    digitalWrite(TRIG_PIN3, LOW);
-    digitalWrite(TRIG_PIN4, HIGH);
-    digitalWrite(TRIG_PIN5, LOW);
-    Serial.println("LED4 ON");
-    Serial.println();
+
+  else if(distance > 50 && distance <= 75){
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, HIGH);
+    digitalWrite(ledPin10, HIGH);
+    digitalWrite(ledPin11, LOW);
   }
-  else if(distance < 177){
-    digitalWrite(TRIG_PIN2, HIGH);
-    digitalWrite(TRIG_PIN3, HIGH);
-    digitalWrite(TRIG_PIN4, HIGH);
-    digitalWrite(TRIG_PIN5, HIGH);
-    Serial.println("ALL LED ON");
-    Serial.println();
+
+  else if(distance > 75){
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, HIGH);
+    digitalWrite(ledPin10, HIGH);
+    digitalWrite(ledPin11, HIGH);
   }
+
   Serial.print(distance);
-  Serail.println(" inch");
-  delayMicroseconds(2);
+  Serial.println(" CM");
+  delay(1000);
 }
